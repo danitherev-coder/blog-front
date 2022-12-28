31029b8fb298151fa2c77d9066ce1b0da33792ba
Carlos Daniel <danitherev@pop-os.localdomain>
2022-12-27 20:54:30 -0500

    jum

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -8,7 +8,7 @@ const Error404 = () => {
   return (
     <>
       <div id="notfound">
-        <div className="notfound">
+        <div className="notfound" >
           <div className="notfound-404">
             <h1>Oops!</h1>
           </div>
